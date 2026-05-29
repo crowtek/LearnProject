71378611ea3e62669cd724279afe625e2f3ff0f5
crowtek <prt3@hotmail.de>
2026-05-29 15:21:03 +0200

    Added a serialized GlobalStoryStateSO reference to StoryTrigger so the trigger can query persistent story progress.

--- Assets/Scripts/StoryManager/Utils/StoryTrigger.cs
+++ Assets/Scripts/StoryManager/Utils/StoryTrigger.cs
@@ -6,11 +6,20 @@ public class StoryTrigger : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private StringEventChannelSO setFlagRequestChannel;
+    [SerializeField] private GlobalStoryStateSO storyState;
 
     [Header("Flag to Set")]
     [StoryFlag] [SerializeField] private string flagToSet;
 
     private bool hasTriggered = false;
+
+    private void Awake()
+    {
+        if (!string.IsNullOrEmpty(flagToSet) && storyState != null)
+        {
+            hasTriggered = storyState.IsFlagCompleted(flagToSet);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
